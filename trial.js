1fc30b9da1513fa0a4472cde12b0c474a8a077f1
Fatesaid <fatesaid56@gmail.com>
2024-09-19 14:17:32 +0200

    reformatting files

--- trial.js
+++ trial.js
@@ -1,11 +1,76 @@
-function sum(a, b) {
-  return a + b;
-}
-
 function capital(string) {
   let word = string.split("");
   word[0] = word[0].toUpperCase();
   return word.join("");
 }
 
-export { sum, capital };
+function reverseString(string) {
+  let word = string.split("").reverse().join("");
+  return word;
+}
+
+const calculator = (function () {
+  const add = (a, b) => a + b;
+  const sub = (a, b) => a - b;
+  const multi = (a, b) => a * b;
+  const div = (a, b) => a / b;
+  return { add, sub, multi, div };
+})();
+
+function caesarCipher(string, num) {
+  let alphabet = createAlphabet();
+
+  let cipher = shift(num);
+
+  string = string.split("");
+
+  function convert() {
+    let encrypted = [];
+    for (let i = 0; i < string.length; i++) {
+      if (/\W/.test(string[i])) {
+        encrypted.push(string[i]);
+      }
+      if (/[A-Z]/.test(string[i])) {
+        let index = alphabet.indexOf(string[i].toLowerCase());
+        encrypted.push(cipher[index].toUpperCase());
+      } else {
+        let index = alphabet.indexOf(string[i]);
+        encrypted.push(cipher[index]);
+      }
+    }
+    return encrypted.join("");
+  }
+
+  function createAlphabet() {
+    let array = [];
+    for (let i = 97; i < 123; i++) {
+      array.push(String.fromCharCode(i));
+    }
+    return array;
+  }
+
+  function shift(num) {
+    let cipher = [];
+    for (let i = num; i < alphabet.length; i++) {
+      cipher.push(alphabet[i]);
+    }
+
+    for (let i = 0; i < num; i++) {
+      cipher.push(alphabet[i]);
+    }
+    return cipher;
+  }
+
+  return convert();
+}
+
+function analyzeArray(array) {
+  const obj = Object(array[3], array[0], array[1], array[5]);
+
+  function Object(average, min, max, length) {
+    return { average, min, max, length };
+  }
+  return obj;
+}
+
+export { capital, reverseString, calculator, caesarCipher, analyzeArray };
--- trial.test.js
+++ trial.test.js
@@ -1,8 +1,10 @@
-import { sum, capital } from "./trial";
-
-test("adds 1 and 2 to equal 3", () => {
-  expect(sum(1, 2)).toEqual(3);
-});
+import {
+  capital,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+} from "./trial";
 
 test("the word is capitalized", () => {
   expect(capital("word")).toMatch("Word");
@@ -10,4 +12,22 @@ test("the word is capitalized", () => {
 
 test("print out string in reverse", () => {
   expect(reverseString("word")).toMatch("drow");
+});
+
+test("calculating operators", () => {
+  expect(calculator.add(1, 2)).toEqual(3);
+  expect(calculator.sub(2, 1)).toEqual(1);
+  expect(calculator.multi(2, 2)).toEqual(4);
+  expect(calculator.div(2, 1)).toEqual(2);
+});
+
+test("print out ceasar cipher", () => {
+  expect(caesarCipher("xyz", 3)).toMatch("abc");
+  expect(caesarCipher("heLLo", 3)).toMatch("khOOr");
+  expect(caesarCipher("Hello, World!", 3)).toMatch("Khoor, Zruog!");
+});
+
+test("return object with following properties", () => {
+  const object = analyzeArray([1, 8, 3, 4, 2, 6]);
+  expect(object).toEqual({ average: 4, min: 1, max: 8, length: 6 });
 });
